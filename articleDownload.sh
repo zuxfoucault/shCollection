f38493c1cb5d329420424a1e79055db5c0c4eb5d
zuxfoucault <zuxfoucault@_>
2014-01-23 01:36:47 +0800

    modify articleDownload.sh; add extension .pdf

--- articleDownload.sh
+++ articleDownload.sh
@@ -1,7 +1,7 @@
 if [ "$#" -ne "0" ]; then
 	curl -O $1
 	if [ "$#" -gt "1" ]; then
-		mv `find . -ctime -2s` "$2"
-		rename 's/ /_/g' "$2"
+		mv `find . -ctime -2s` "$2".pdf
+		rename 's/ /_/g' "$2".pdf
 	fi
 fi
